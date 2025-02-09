9b9d17acca4e20cd16d42b0cc465812f1ace71ef
jérémy Fortin <2142667@cegeprdl.ca>
2025-02-08 19:26:11 -0500

    correction bug

---
+++ TpCegepWeb/Controllers/CoursController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TpCegepWeb.Controllers
+{
+    public class CoursController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
